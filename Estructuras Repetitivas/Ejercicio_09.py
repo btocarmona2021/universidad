615ce078cf6d98bbffce148a8ebd432250ba0890
Alberto <btocarmona@gmail.com>
2024-05-10 11:29:54 -0300

    Primer Commit

--- Estructuras Repetitivas/Ejercicio_09.py
+++ Estructuras Repetitivas/Ejercicio_09.py
@@ -10,21 +10,4 @@ distintos números enteros por teclado hasta ingresar el número (0) que actuar�
 
 """
 
-
-#
-# def numero_primo(valor):
-#     if  valor / valor == 1
-#
-
-def es_primo(n):
-    if n <= 1:
-        return False
-
-    for i in range(2, n):
-        if n % i == 0:
-            return False
-
-    return True
-
-
-print(es_primo(4))
+#recalculando 😂
